582c12dd7a6b747b5b768df3f28b25c95910dbc9
Orlov Dmitriy <orlovdmi.igor@gmail.com>
2023-10-18 16:26:55 +0300

    feat(Bun) Init bun post install

--- StarterKitPostInstall.php
+++ StarterKitPostInstall.php
@@ -1,9 +1,32 @@
 <?php
+use Symfony\Component\Process\Process;
 
 class StarterKitPostInstall
 {
     public function handle($console)
     {
+        $this->executeCommand('npm install -g bun');
+        $this->executeCommand('bun init');
+
         $console->line('Statamic starter kit installed successfully!');
+    }
+
+    /**
+     * Execute a shell command.
+     *
+     * @param string $command The shell command to execute.
+     *
+     * @return void
+     */
+    protected function executeCommand($command)
+    {
+        $process = Process::fromShellCommandline($command, base_path());
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            $this->error('Error executing command: ' . $command);
+            $this->error($process->getErrorOutput());
+            exit(1);
+        }
     }
 }
